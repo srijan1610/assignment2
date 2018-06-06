93b4d57a129de2d07685f09739dc578b3597b4e7
srijan1610 <srijanraj23@gmail.com>
2018-06-07 00:10:56 +0530

    assignment2

---
+++ a2.py
@@ -0,0 +1,17 @@
+print("hello")
+print("acad"+"view")
+x="srijan"
+y="bihar"
+z=20
+print("hi,my name is %s,i am from%s,my age is %d"%(x,y,z))
+
+print('let\'s) get started')
+
+s="acadview"
+course="python"
+fees=5000
+print("thanks for choosing %s,your course is%s,your fees is%d"%(s,course,fees))
+
+name="Tony stark"
+salary=1000000
+print("%s %d"%(name,salary))
